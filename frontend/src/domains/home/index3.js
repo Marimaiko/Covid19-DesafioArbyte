1a01c8876f4a34153bda72d5391ad54c9fda258a
MarianaMaiko <marianamaikoht@gmail.com>
2020-08-10 21:22:50 -0300

    Backend funcionando

--- frontend/src/domains/home/index3.js
+++ frontend/src/domains/home/index3.js
@@ -21,29 +21,38 @@ async function getData()
   const dataInicio = `${ano}-${zeroPadding(mes+1)}-${zeroPadding(dia-1)}T00:00:00Z`;
 
   const filtroData = `from=${dataInicio}&to=${dataFim}`;
+  // const url =`https://api.covid19api.com/country/${country}/status/confirmed?${filtroData}`
   const url =`https://api.covid19api.com/total/country/${country}/status/confirmed?${filtroData}`
+
+
+  
   try {
     const res = await axios.get(url);
     // const res = await axios.get(`https://api.covid19api.com/total/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z`);
 
     const data = res.data;
 
-    const confirmed = data.Confirmed;
 
     const goCountry = document.getElementById('country')
-
-    goCountry.innerHTML = data.Country;
+    goCountry.innerHTML = `<br><br><br>País selecionado: ${data[0].Country}`;
   
     const goCountryCode = document.getElementById('countryCode')
-    goCountryCode.innerHTML = data.CountryCode;
+    goCountryCode.innerHTML = `Código do País: ${data[0].CountryCode}` ;
 
     const goConfirmed = document.getElementById('casesConfirmed')
-    goConfirmed.innerHTML = confirmed;
+    goConfirmed.innerHTML = `Número de casos: ${data[0].Cases}` ;
+
+    const goDate = document.getElementById('lastDate')
+    goDate.innerHTML = `Data da última atualização:<br> ${data[0].Date}`;
+
 
   }
   catch (error)  {
     console.log('Veeesh...deu ruim', error) 
-}
+
+}    
+
+
 }
 
 
@@ -54,6 +63,5 @@ const info = `O primeiro caso da pandemia pelo <a href="https://pebmed.com.br/ca
 Em fevereiro, a transmissão da Covid-19, nome dado à doença causada pelo SARS-CoV2, no Irã e na Itália chamaram a atenção pelo crescimento rápido de novos casos e mortes, fazendo com que o Ministério da Saúde alterasse a definição de caso suspeito para incluir pacientes que estiveram em outros países. No mesmo dia,<a href="https://pebmed.com.br/ministerio-da-saude-confirma-primeiro-caso-de-coronavirus-no-pais/">o primeiro caso do Brasil foi identificado</a> , em São Paulo.
 
 Em março, a Organização Mundial da Saúde (OMS) definiu o surto da doença como pandemia. Poucos dias depois, foi confirmada a primeira morte no Brasil, em São Paulo. No mesmo dia, dois pacientes que haviam testado positivo para coronavírus, do Rio de Janeiro, vieram a óbito, mas laudos das mortes ainda não foram divulgados`;
-
 const infoCovid = document.getElementById('info-covid')
 infoCovid.innerHTML=info;
